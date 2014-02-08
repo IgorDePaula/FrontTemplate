acca9720788c198c33741d915cf3b6fd1a812ce6
Igor C. de Paula <igordepaula@adminweb.com.br>
2014-02-08 21:02:00 -0200

    Ready for use

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+module.exports = function(grunt) {
+    'use strict';
+    var gruntConfig = {
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            add_banner: {
+                options: {
+                    banner: '/**\n' +
+                            ' * <%= pkg.name %> - v<%= pkg.version %>\n' +
+                            ' * <%= pkg.description %>\n' +
+                            ' * <%= grunt.template.today("dd-mm-yyyy") %> \n'+
+                            ' */',
+                    keepSpecialComments: '*',
+                    report: 'min',
+                },
+                files: {
+                    'public_html/css/all.min.css': ['src/css/*.css']
+                }
+            }
+        },
+        coffee: {
+            compileJoined: {
+                options: {
+                    join: true
+                },
+                files: {
+                    'public_html/js/all.min.js': 'src/coffee/start.coffee'
+
+                }
+            },
+            glob_to_multiple: {
+                expand: true,
+                flatten: true,
+                cwd: 'src/coffee/',
+                src: ['*.coffee'],
+                dest: 'public_html/js/',
+                ext: '.js'
+            },
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["src/less"],
+                    banner: '/**\n' +
+                            ' * <%= pkg.name %> - v<%= pkg.version %>\n' +
+                            ' * <%= pkg.description %>\n' +
+                            ' * <%= grunt.template.today("dd-mm-yyyy") %> \n'+
+                            ' */'
+                },
+                files: {
+                    "src/css/style.css": "src/less/style.less"
+                }
+            },
+            production: {
+                options: {
+                    paths: ["public_html/css"],
+                    cleancss: true,
+                    compress: true,
+                    ieCompat: true,
+                    report: 'min'
+                },
+                files: {
+                    "public_html/css/all.css": "src/less/style.less"
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'src/less/*.less',
+                tasks: ['less', 'cssmin'],
+                options: {
+                    livereload: true
+                }
+            },
+            jade: {
+                tasks: ['jade'],
+                files: ['src/jade/nav.jade', 'src/jade/index.jade']
+            },
+            coffee: {
+                tasks: ['coffee'],
+                files: ['src/coffee/start.coffee']
+            },
+            uglify:{
+                tasks: ['uglify'],
+                files: ['public_html/js/*.js','!public_html/js/all.min.js']
+            }
+        },
+        jade: {
+            compile: {
+                options: {
+                    client: false,
+                    pretty: true
+                },
+                files: {
+                    'public_html/nav.html': 'src/jade/nav.jade',
+                    'public_html/index.html': 'src/jade/index.jade'
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/**'+
+                        ' * <%= pkg.name %> - v<%= pkg.version %> \n' +
+                        ' * <%= pkg.description %>\n' +
+                        ' * <%= grunt.template.today("dd-mm-yyyy") %> \n'+
+                        ' */\n'
+            },
+            my_target: {
+                files: {
+                    'public_html/js/all.min.js': ['public_html/js/*.js','!public_html/js/all.min.js']
+                }
+            }
+        }
+
+    };
+    grunt.initConfig(gruntConfig);
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.registerTask('default', ["watch"]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 FrontTemplate
 =============
+
+This is a basic front end Template with [less](http://lesscss.org/ "less"), [jade](http://jade-lang.com/ "jade") and [coffee script](http://coffeescript.org/ "coffee script") implemented on [grunt](http://gruntjs.com/ "grunt").
+
+The tasks are automated with grunt to parse the scripts from source code to compressed files ready to use. Change the need paths relativo for your project.
